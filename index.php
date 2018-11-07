60fbe008445a532b99f847e55e71c1cff84788c1
yuxuyang663 <44663987+yuxuyang663@users.noreply.github.com>
2018-11-07 18:39:25 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,94 +1,36 @@
 <?php 
-// $sum=0;
-// for($i=1;$i<=100;$i++) {
-// $sum+=$i;
-// }
-// echo $sum;
-
-// $sum= $i = 0;
-// while( $i<= 100 ) {
-// $sum += ( $i++ );
-// }
-// echo $sum;
-
-// function get_sum($n){
-// return $n==1?1:$n+get_sum($n-1);
-// }
-// echo get_sum(100);
-
-#include <stdio.h>
-// int func(int n)
-// {
-//     if(n == 1)
-//     {
-//         return 1;
-//     }
-//     else if(n > 0)
-//     {
-//         return (n * func(n - 1));
-//     }
-//     else
-//     {
-//         printf("error\n");
-//     }
-// }
-// int main()
-// {
-//     int n;
-//     int s;
-//     printf("please input n:\n");
-//     scanf("%d",&n);
-//     s = func(n);
-//     printf("%d!=%d\n",n,s);
-//     return 0;
-// }
-
-#include <stdio.h>
-// int main()
-// {
-//     int n;
-//     int i;
-//     int s = 1;
-//     printf("please input n:\n");
-//     scanf("%d",&n);
-//     printf("%d!=",n);
-//     for(i = n; i > 1; i--)
-//     {
-//         s *= i;
-//         printf("%d*",i);
-//     }
-//     printf("1=%d\n",s);
-//     return 0;
-// }
-
-// $string="abba";
-//   $stra=array();
-//   $strb=array();
-//   $res=checkhuiwen($string);
-//   function checkhuiwen($string){
-//        //输入字符串转换为数组
-//      for($i=0;$i<strlen($string);$i++){
-//         $stra[$i]=$string[$i];
-//       }
-//        //逆序输出数组array_reverse
-//        $strb=array_reverse($stra);
-//        //判断两数组是否相等，相等为回文
-//       for($i=0;$i<count($stra);$i++){
-//        if($stra[$i]!=$strb[$i]){
-//           return 1;
-//           break;
-//        }     
-//   }
-//   return 2;
-//   }
-//   if($res==2){
-//     echo "YES";
-//   }else{
-//     echo "NO!";
-//   }
-
-　　class Person{
-	public $name;
-	public $age;
-　　}
-?>
+echo ugly(14);
+function ugly($ugly)
+{
+    if($ugly==1)return 1;
+    if(getUgly($ugly)==false)return "不是丑数";
+    $j = 1;
+    for($i=2;;$i++)
+    {
+        $a = getUgly($i);
+        if($a){
+            $j++;
+        }
+        if($i==$ugly){
+            return $j;
+        }
+    }
+}
+function getUgly($ugly)
+{
+   if($ugly<=0) return false;
+   while($ugly!=1)
+   {
+       while($ugly%2==0)
+       {
+           $ugly = $ugly/2;
+       }
+       while($ugly%3==0){
+           $ugly = $ugly/3;
+       }
+       while($ugly%5==0){
+           $ugly = $ugly/5;
+       }
+       return $ugly==1?true:false;
+   }
+}
