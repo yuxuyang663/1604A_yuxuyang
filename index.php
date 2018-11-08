5be9a9254e63e67727209d14322b1dde03c0877c
yuxuyang663 <44663987+yuxuyang663@users.noreply.github.com>
2018-11-08 15:50:20 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,36 +1,21 @@
 <?php 
-echo ugly(14);
-function ugly($ugly)
+function sixOne($n,$m)
 {
-    if($ugly==1)return 1;
-    if(getUgly($ugly)==false)return "不是丑数";
-    $j = 1;
-    for($i=2;;$i++)
-    {
-        $a = getUgly($i);
-        if($a){
-            $j++;
+    echo "<pre>";
+    $a = 1;
+    $arr = range(0,$n-1);
+    while(true){
+        if(count($arr)==1){
+            return array_keys($arr)[0];
         }
-        if($i==$ugly){
-            return $j;
+        foreach($arr as $key=>$val){
+            $a++;
+            if($a==$m-1){
+                unset($arr[$key]);
+                $a=0;
+            }
         }
     }
 }
-function getUgly($ugly)
-{
-   if($ugly<=0) return false;
-   while($ugly!=1)
-   {
-       while($ugly%2==0)
-       {
-           $ugly = $ugly/2;
-       }
-       while($ugly%3==0){
-           $ugly = $ugly/3;
-       }
-       while($ugly%5==0){
-           $ugly = $ugly/5;
-       }
-       return $ugly==1?true:false;
-   }
-}
+
+?>
