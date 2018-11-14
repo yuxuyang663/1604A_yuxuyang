59902f48e41e34eeeb669d84e9f9b1a4c38eb647
yuxuyang663 <44663987+yuxuyang663@users.noreply.github.com>
2018-11-14 08:57:44 +0800

    Add files via upload

--- show.php
+++ show.php
@@ -1,35 +1,20 @@
-<?php 
-//单例
-class Uni{
-        //创建静态私有的变量保存该类对象
-    static private $instance;
-        //参数
-    private $config;
-        //防止直接创建对象
-    private function __construct($config){
-        $this -> config = $config;
-                echo "我被实例化了";
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 于旭阳
+ * Date: 2018/11/14
+ * Time: 8:29
+ */
+function asd($num1,$num2)
+{
+    while ($num2!=0)
+    {
+        $temp = $num1^$num2;
+        $num2 = ($num1 & $num2)<<1;
+        $num1 = $temp;
     }
-        //防止克隆对象
-    private function __clone(){
 
-    }
-    static public function getInstance($config){
-                //判断$instance是否是Uni的对象
-                //没有则创建
-        if (!self::$instance instanceof self) {
-            self::$instance = new self($config);
-        }
-        return self::$instance;
-        
-    }
-    public function getName(){
-        echo $this -> config;
-    }
+    return $num1;
 }
-$db1 = Uni::getInstance(1);
-$db1 -> getName();
-echo "<br>";
-$db2 = Uni::getInstance(4);
-$db2 -> getName();
-?>
+echo asd(100,1);
+
