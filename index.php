e8eccfe67f92ca973ecbbcf6937f29edd3feeb43
yuxuyang663 <44663987+yuxuyang663@users.noreply.github.com>
2018-11-20 08:34:21 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: 于旭阳
- * Date: 2018/11/16
- * Time: 8:23
+ * User: 奶糖i
+ * Date: 2018/11/20
+ * Time: 8:25
  */
 
-function First($str){
-$sr = chunk_split($str,1,',');
-$sr = rtrim($sr,',');
-$arr = explode(',',$sr);
-$cnt = array_count_values($arr);
-foreach ($cnt as $key=>$val){
-    if ($val==1){
-        return stripos($str,$key);
+function NumberOf1($n)
+{
+    $count = 0;
+    $flag = 1;
+    while ($flag != 0) {
+        if (($n & $flag) != 0) {
+            $count++;
+        }
+        $flag = $flag << 1;
     }
+    return $count;
 }
-return -1;
-}
+
+
+//测试
+$num=35;
+echo $num."的二进制是".decbin($num)."<br/>";
+echo $num."共有".NumberOf1($num)."个1";
+
